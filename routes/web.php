379d36390db3812327ec6cbca9618241def898dd
Jānis Gitendorfs <janis.gitendorfs@gmail.com>
2020-11-25 11:08:56 +0200

    -

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('articles', 'ArticlesController');
+Route::resource('articles', 'ArticlesController')
+    ->middleware(['auth'])
+    ->except(['index', 'show']);
+
+Route::get('articles', 'ArticlesController@index')->name('articles.index');
+Route::get('articles/{article}', 'ArticlesController@show')->name('articles.show');
--- tests/Feature/ArticlesControllerTest.php
+++ tests/Feature/ArticlesControllerTest.php
@@ -32,6 +32,32 @@ class ArticlesControllerTest extends TestCase
         ]);
     }
 
+    public function testUpdateExistingArticle(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->followingRedirects();
+
+        $response = $this->put(route('articles.update', $article), [
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('articles', [
+            'id' => $article->id,
+            'user_id' => $user->id,
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+    }
+
     public function testDeleteArticle(): void
     {
         $user = User::factory()->create();
@@ -57,5 +83,43 @@ class ArticlesControllerTest extends TestCase
             'title' => $article->title,
             'content' => $article->content
         ]);
+    }
+
+    public function testRedirectWhenUnauthorizedTriesToSeeCreateForm(): void
+    {
+        $response = $this->get(route('articles.create'));
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testRedirectWhenUnauthorizedTriesToCreateArticle(): void
+    {
+        $response = $this->post(route('articles.store'), [
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testRedirectWhenUnauthorizedTriesToSeeUpdateForm(): void
+    {
+        $article = Article::factory()->create();
+
+        $response = $this->get(route('articles.edit', $article));
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testRedirectWhenUnauthorizedTriesToUpdateArticle(): void
+    {
+        $article = Article::factory()->create();
+
+        $response = $this->put(route('articles.update', $article), [
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
     }
 }
